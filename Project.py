5a156d14bd872a960374fe4da8762de821af5959
Isaac Asare <iasarekwaku@gmail.com>
2021-07-19 18:19:55 +0000

    initial commit

---
+++ Project.py
@@ -0,0 +1,102 @@
+import requests
+import json
+import pandas as pd
+import os
+from sqlalchemy import create_engine
+import string
+
+def remove(string):
+        escapes = ''.join([chr(char) for char in range(1, 32)])
+        translator = str.maketrans('', '', escapes)
+        t = string.translate(translator)
+        return t
+        
+def get_API_data(choice):    
+    
+        secret = '	I68I7hy1Rpww0cGWAVjtsoi7dO5fxg'
+        personal_use_script = 'E2xxiNzl8utDycr7e_dGIg'
+
+
+        # note that CLIENT_ID refers to 'personal use script' and SECRET_TOKEN to 'token'
+        auth = requests.auth.HTTPBasicAuth('E2xxiNzl8utDycr7e_dGIg', 'I68I7hy1Rpww0cGWAVjtsoi7dO5fxg')
+
+        # here we pass our login method (password), username, and password
+        data = {'grant_type': 'password',
+                'username': 'dpbolat19',
+                'password': 'Codegram101!'}
+
+        # setup our header info, which gives reddit a brief description of our app
+        headers = {'User-Agent': 'MyBot/0.0.1'}
+
+        # send our request for an OAuth token
+        res = requests.post('https://www.reddit.com/api/v1/access_token',
+                            auth=auth, data=data, headers=headers)
+
+        # convert response to JSON and pull access_token value
+
+        TOKEN = res.json()['access_token']
+
+        # add authorization to our headers dictionary
+        headers = {**headers, **{'Authorization': f"bearer {TOKEN}"}}
+
+        # while the token is valid (~2 hours) we just add headers=headers to our requests
+        response = requests.get('https://oauth.reddit.com/r/'+ choice + '/hot', headers=headers)
+        
+        df = pd.DataFrame()  # initialize dataframe
+        # loop through each post retrieved from GET request
+        for post in response.json()['data']['children'][2:]:
+            # append relevant data to dataframe
+            #clean = re.sub(r"[^. 0-9a-z]", "", post['data']['selftext'], re.IGNORECASE)
+            df = df.append({
+                'subreddit': post['data']['subreddit'],
+                'title': post['data']['title'],
+                'selftext': remove(post['data']['selftext']),
+                'author_fullname':post['data']['author_fullname']
+            }, ignore_index=True) 
+        return df
+            
+        
+def createdb(name):
+    df = get_API_data(name)
+    os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS ' + 'Codegram' + ';"')
+    #create_engine('mysql+mysqldb:///mydb?charset=utf8&use_unicode=1')
+    engine = create_engine('mysql://root:codio@localhost/Codegram?charset=utf8mb4&use_unicode=1')
+    with engine.connect() as con:
+            con.execute('ALTER DATABASE Codegram COLLATE = "utf8mb4_unicode_ci";')
+    df.to_sql(name, con=engine, if_exists='replace', index=False)
+    return df
+
+def savedb(name):
+    os.system("mysqldump -u root -pcodio " + name +' > Codegram.sql')
+
+    
+def loaddb(name):
+    os.system('mysql -u root -pcodio -e "CREATE DATABASE IF NOT EXISTS ' +
+              name + ';"')
+    os.system("mysql -u root -pcodio Premier_League < Codegram.sql")
+
+
+def loadDataset(name, update=False):
+    engine = create_engine('mysql://root:codio@localhost/' + 'Codegram')
+    loaddb(name)
+    df = pd.read_sql_table(name, con=engine)
+    return df
+
+        
+option_1 = "cscareerquestions"   
+option_2 = "csMajors"
+option_3 = "csinterviewproblems"
+option_4 = 'DataScienceJobs'
+option_5 = 'cscareers'
+option_6 = 'datasciencecareers'
+
+DB_list = [option_1, option_2, option_3, option_4, option_5, option_6]
+for i in DB_list:
+        createdb(i)
+        savedb(i)
+#print(db.head())
+
+    
+
+
+
---
+++ main.py
@@ -0,0 +1,106 @@
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_bcrypt import Bcrypt
+import json
+from Project import *
+from forms import *
+from flask_behind_proxy import FlaskBehindProxy
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+bcrypt = Bcrypt(app)
+app.config['SECRET_KEY'] = 'a7b24667c02b1eeecea744c063db3bd3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:codio@localhost/Codegram'
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# this variable, db, will be used for all SQLAlchemy commands
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit(): # checking if entries are valid
+        try:
+            pw_hash = bcrypt.generate_password_hash(form.password.data.encode('utf-8'))
+            user = User(username=form.username.data, email=form.email.data, password=pw_hash)
+            db.session.add(user)
+            db.seesion.commit()
+        except Exception as e:
+            flash(f'Your account could not be created. Due to {e}')
+            return render_template('register.html', title="SignUp Page", form=form)
+        else:
+            flash(f'Account created for {form.username}')
+            return render_template('register.html', title="SignUp Page", form=form)
+    return render_template('register.html', title="SignUp Page", form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login Page', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0")
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+# NOTHING BELOW THIS LINE NEEDS TO CHANGE
+# this route will test the database connection and nothing more
+# @app.route('/')
+# def testdb():
+#     try:
+#         db.session.query(text('1')).from_statement(text('SELECT 1')).all()
+#         return '<h1>It works.</h1>'
+#     except Exception as e:
+#         # e holds description of the error
+#         error_text = "<p>The error:<br>" + str(e) + "</p>"
+#         hed = '<h1>Something is broken.</h1>'
+#         return hed + error_text
