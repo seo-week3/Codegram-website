84190a8344b9df2edab6230ceb0d6d26c30e19e9
Ruramai Mutefura <RMutefura.23@westminster-mo.edu>
2021-07-21 19:02:33 +0000

    fixed sytling

--- forms.py
+++ forms.py
@@ -1,12 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField
+from wtforms import SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length
+from wtforms.validators import Email, EqualTo, ValidationError
+
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email =  StringField('Email',validators=[DataRequired(), Email()])
+    username = StringField(
+        'Username', validators=[
+            DataRequired(), Length(
+                min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField(
+        'Confirm Password', validators=[
+            DataRequired(), EqualTo('password')])
     submit = SubmitField('SignUp')
 
 
@@ -15,12 +23,21 @@ class LoginForm(FlaskForm):
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
-    submit = SubmitField('Login') 
-    
+    submit = SubmitField('Login')
+
+
 class PostForm(FlaskForm):
-    subtitle = StringField('Subtopic', validators=[DataRequired(), Length(min=2, max=20)])
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    title = StringField('Title', validators=[DataRequired(), Length(min=1, max=30)])
+    subtitle = StringField(
+        'Subtopic', validators=[
+            DataRequired(), Length(
+                min=2, max=20)])
+    username = StringField(
+        'Username', validators=[
+            DataRequired(), Length(
+                min=2, max=20)])
+    title = StringField(
+        'Title', validators=[
+            DataRequired(), Length(
+                min=1, max=30)])
     text = TextAreaField('Post', validators=[DataRequired(), Length(max=200)])
     submit = SubmitField('Post')
-    
